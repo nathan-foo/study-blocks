a2e9d2f2cab86ed6c15b2d45a8f121a40c57b83a
Nathan Foo <ntfoo27@outlook.com>
2024-12-27 15:21:50 -0600

    Fix navbar to top

--- client-next/app/(pages)/_components/Navbar.jsx
+++ client-next/app/(pages)/_components/Navbar.jsx
@@ -1,16 +1,30 @@
 "use client"
 
 import { UserButton, useUser } from '@clerk/nextjs'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
 const Navbar = () => {
     const { user } = useUser();
 
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
   return (
-    <div className='py-4 px-6'>
-        <div className='flex items-center justify-end gap-x-3 ml-auto'>
+    <div className={`fixed bg-white top-0 left-0 w-full py-4 px-6 ${isScrolled ? 'shadow-md' : 'shadow-none'} transition-shadow duration-300`}>
+        <div className='container mx-auto flex items-center justify-end gap-x-3'>
             <Link href='/'>
                 <Button size='sm' variant='ghost'>
                     Home

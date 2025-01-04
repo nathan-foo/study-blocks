13490b382f698fc13619f40aa28ed5d433a1907c
Nathan Foo <ntfoo27@outlook.com>
2025-01-03 23:34:15 -0600

    Basic flashcard functionality

--- client-next/app/(pages)/(routes)/dashboard/[id]/flashcards/page.jsx
+++ client-next/app/(pages)/(routes)/dashboard/[id]/flashcards/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
@@ -9,32 +10,56 @@ const FlashcardPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        if (!params) return;
-        const { id } = await params;
+      if (!params) return;
+      const { id } = await params;
 
-        const url = `${process.env.NEXT_PUBLIC_API_URL}/api/blocks/${id}`;
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/api/blocks/${id}`;
 
-        try {
-            const response = await fetch(url, {
-                cache: 'no-store'
-            });
+      try {
+        const response = await fetch(url, {
+          cache: 'no-store'
+        });
 
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-
-            const data = await response.json();
-            setBlock(data.block);
-        } catch (error) {
-            throw new Error(`Failed to create block: ${error}`);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
         }
+
+        const data = await response.json();
+        setBlock(data.block);
+      } catch (error) {
+        throw new Error(`Failed to create block: ${error}`);
+      }
     }
     fetchData();
-}, []);
+  }, []);
 
   return (
-    <div className='pt-16'>
-      {block && (JSON.stringify(block.flashcards.set))}
+    <div>
+      {/* {block && (JSON.stringify(block.flashcards.set))} */}
+      {block && (
+        <div className='flex items-center justify-center h-screen'>
+          <div className='w-[50%]'>
+            <Carousel opts={{ loop: true }}>
+              <CarouselContent>
+                {block.flashcards.set.map((flashcard, index) => (
+                  <CarouselItem key={index}>
+                    <div className='px-16 py-32 border rounded-lg text-center text-lg'>
+                      <p>
+                        <b>Q:</b> {flashcard.question}
+                      </p>
+                      <p className='pt-2'>
+                        <b>A:</b> {flashcard.answer}
+                      </p>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
